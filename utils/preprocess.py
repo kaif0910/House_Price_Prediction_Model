18a47326ed0128c9524d0632cf132ce5f87995dc
kaif0910 <mdkaifperwez@gmail.com>
2026-07-03 18:56:59 +0530

    added the validation split of the dataset

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -20,10 +20,17 @@ def load_data():
     X = df.drop("price", axis=1)
     y = df["price"]
 
-    X_train, X_test, y_train, y_test = train_test_split(
+    X_train, X_temp, y_train, y_temp = train_test_split(
         X,
         y,
-        test_size=0.2,
+        test_size=0.3,
+        random_state=42
+    )
+
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_temp,
+        y_temp,
+        test_size=0.5,
         random_state=42
     )
 
@@ -31,9 +38,11 @@ def load_data():
 
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
+    X_val = scaler.transform(X_val)
 
     X_train = torch.tensor(X_train, dtype=torch.float32)
     X_test = torch.tensor(X_test, dtype=torch.float32)
+    X_val = torch.tensor(X_val, dtype=torch.float32)
 
     y_train = torch.tensor(
         y_train.values,
@@ -45,10 +54,17 @@ def load_data():
         dtype=torch.float32
     ).unsqueeze(1)
 
+    y_val = torch.tensor(
+        y_val,
+        dtype=torch.float32
+    ).unsqueeze(1)
+
     return (
         X_train,
         X_test,
+        X_val
         y_train,
         y_test,
+        y_val,
         scaler
     )
